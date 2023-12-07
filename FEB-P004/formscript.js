f8ff95d7dec71c376954c1768371dc7ae552aa2a
Diogo Dorea <diogodorea@gmail.com>
2023-12-06 23:05:16 -0300

    review3

--- FEB-P004/formscript.js
+++ FEB-P004/formscript.js
@@ -1,27 +1,27 @@
 function capturarInformacoes() {
-    const nomeDestino = document.getElementById('imagem').value;
-    const duracao = document.getElementById('cidade').value;
-    const preco = document.getElementById('valor').value;
-    const imagemUrl = document.getElementById('pacote').value;
+    const cidade = document.getElementById('cidade').value;
+    const diarias = document.getElementById('diarias').value;
+    const valor = document.getElementById('valor').value;
+    const URL_Imagem = document.getElementById('imagem').value;
 
-    console.log(imagemUrl); 
+    console.log(URL_Imagem); 
 
     const novoDestino = document.createElement('div');
     novoDestino.className = 'card';
 
     novoDestino.innerHTML = `
         <header>
-            <img src="${imagemUrl}" alt="Imagem do Destino">
-            <h3>${nomeDestino}</h3>
+            <img src="${URL_Imagem}" alt="Imagem do Destino">
+            <h3>${cidade}</h3>
         </header>
         <main>
             <ul>
                 <li>Áereo ida e volta:</li>
-                <li>Duração: ${duracao} diárias</li>
+                <li>Duração: ${diarias} diárias</li>
                 <li>Café da manhã:</li>
             </ul>
             <div>
-                <h2>Preço: R$ ${preco}</h2>
+                <h2>Preço: R$ ${valor}</h2>
                 <span>Taxas inclusas</span>
                 <span>Em até 10x sem Juros</span>
             </div>
@@ -31,9 +31,8 @@ function capturarInformacoes() {
     const containerDestinos = document.getElementById('Destino');
     containerDestinos.appendChild(novoDestino);
 
-    // Limpa os campos do formulário
-    document.getElementById('nomeDestino').value = '';
-    document.getElementById('duracao').value = '';
-    document.getElementById('preco').value = '';
     document.getElementById('imagem').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('valor').value = '';
+    document.getElementById('diarias').value = '';
 }
