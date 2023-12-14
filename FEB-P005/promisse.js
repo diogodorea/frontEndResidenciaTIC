853bae7ecc7b88ccd38a9ad621030a70755a85f3
Diogo Dorea <diogodorea@gmail.com>
2023-12-14 00:18:08 -0300

    review1

--- FEB-P005/promisse.js
+++ FEB-P005/promisse.js
@@ -1,42 +1,42 @@
-// Função para fazer a requisição à API de notícias
-function getNews() {
-    return new Promise((resolve, reject) => {
-        // Substitua a URL da API pela URL real da sua fonte de notícias
-        fetch('https://servicodados.ibge.gov.br/api/v3/noticias/?qtd=3')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Não foi possível obter as notícias.');
-                }
-                return response.json();
-            })
-            .then(data => {
-                resolve(data); // Retorna os dados da API
-            })
-            .catch(error => {
-                reject(error); // Retorna o erro, caso ocorra
+        // Função para fazer a requisição à API de notícias do IBGE
+        function getIBGENews() {
+            return new Promise((resolve, reject) => {
+                fetch('https://servicodados.ibge.gov.br/api/v3/noticias/')
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Não foi possível obter as notícias do IBGE.');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        resolve(data); // Retorna os dados da API
+                    })
+                    .catch(error => {
+                        reject(error); // Retorna o erro, caso ocorra
+                    });
             });
-    });
-}
+        }
 
-// Função para exibir as notícias na página
-function showNews() {
-    getNews()
-        .then(newsData => {
-            // Ajuste esta parte de acordo com a estrutura dos dados da sua API
-            const noticiasHTML = newsData.map(noticia => `
-                <li>
-                    <h3>${noticia.titulo}</h3>
-                    <p>${noticia.introducao}</p>
-                </li>
-            `).join('');
+        // Função para exibir as notícias na página
+        function showIBGENews() {
+            getIBGENews()
+                .then(newsData => {
+                    // Ajuste esta parte de acordo com a estrutura dos dados da API
+                    const noticiasHTML = newsData.map(noticia => `
+                        <li>
+                            <h3>${noticia.titulo}</h3>
+                            <p>${noticia.resumo}</p>
+                            <p>Data de publicação: ${noticia.dataPublicacao}</p>
+                        </li>
+                    `).join('');
 
-            // Seleciona a lista de notícias e insere o conteúdo obtido da API
-            document.querySelector('.apinoticia div').innerHTML = noticiasHTML;
-        })
-        .catch(error => {
-            console.error('Erro ao carregar notícias:', error);
-        });
-}
+                    // Seleciona a lista de notícias e insere o conteúdo obtido da API
+                    document.querySelector('.noticias ul').innerHTML = noticiasHTML;
+                })
+                .catch(error => {
+                    console.error('Erro ao carregar notícias do IBGE:', error);
+                });
+        }
 
-// Chamada para exibir as notícias ao carregar a página
-document.addEventListener('DOMContentLoaded', showNews);
+        // Chamada para exibir as notícias ao carregar a página
+        document.addEventListener('DOMContentLoaded', showIBGENews);
