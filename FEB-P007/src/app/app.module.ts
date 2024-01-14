f966f4ceda22bd5693f906aad8617e4091995b46
Diogo Dorea <diogodorea@gmail.com>
2024-01-14 18:24:45 -0300

    modulos base criados e exibidos no app componente

--- FEB-P007/src/app/app.module.ts
+++ FEB-P007/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileReaderComponent } from './file-reader/file-reader.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { VeiculosComponent } from './veiculos/veiculos.component';
+import { EspecificacaoComponent } from './especificacao/especificacao.component';
+import { ExibicaoComponent } from './exibicao/exibicao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileReaderComponent,
+    CategoriasComponent,
+    VeiculosComponent,
+    EspecificacaoComponent,
+    ExibicaoComponent
   ],
   imports: [
     BrowserModule,
---
+++ FEB-P007/src/app/file-reader/file-reader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-reader',
+  templateUrl: './file-reader.component.html',
+  styleUrl: './file-reader.component.css'
+})
+export class FileReaderComponent {
+
+}
